30ba01424bf455e31739606f1ab382e9e6c3ecac
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-05-01 15:16:02 -0400

    flyer added

---
+++ src/components/Question.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Question() {
+    const arrayQuestion = [{question: "", ans:""}, {question:"", ans:""}, {question:"", ans:""}]
+  return (
+    <div>Question</div>
+  )
+}
+
+export default Question
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./Home.css";
-
+import recycle from "../images/recycle.png"
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <p>College campuses are a significant source of solid municipal waste, from containers and paper disposed of by students to cardboard boxes and technology disposed of by staff. As such, in order to reduce its environmental impact, City College needs to continue and focus on a successful solid waste management program.</p>
+        <section>
+            <img src={recycle} alt="recycle"/>
+        </section>
+    </div>
   )
 }
 
